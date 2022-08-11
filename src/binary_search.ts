8e1d932c3eeecd54a8c9cfba4e2a34865cd2addc
Daniel Corcoran <danielc.92@outlook.com>
2022-08-11 10:11:41 +1000

    binary search examples

---
+++ src/binary_search.ts
@@ -0,0 +1,31 @@
+
+const binarySearch = (sortedList: number[], item: number): number | null => {
+    console.log("[start]")
+    
+    let low = 0
+    let high = sortedList.length - 1
+
+    while (low <= high) {
+        let mid = Math.floor((low + high)/2)
+        let guess = sortedList[mid]
+            console.log(low, mid, high)
+        if (guess === item) {
+            return mid
+        }
+
+        if (guess > item) {
+            high = mid - 1
+        } else {
+            low = mid + 1
+        }
+    }
+
+    return null
+}
+
+console.log(binarySearch([1], 1))
+console.log(binarySearch([1,2,3,4,5], 4))
+console.log(binarySearch([1,3,4,5, 25], 25))
+console.log(binarySearch([1,2,3,4,5], 3))
+console.log(binarySearch([1,2,3,4,5], 6))
+console.log(binarySearch(new Array(240000).fill(null).map((_item, index) => index), 165000))
