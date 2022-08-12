4aaf3aaef6235c975e0c728f65fcba70ac2db6cb
daniel corcoran <danielc.92@outlook.com>
2022-08-13 09:40:56 +1000

    lint

--- src/recursion/boxes.ts
+++ src/recursion/boxes.ts
@@ -4,45 +4,45 @@ interface ToyBox {
     boxes: ToyBox[]
 }
 
-const toybox: ToyBox= {
-    name: "toys",
-    hasKey: false,
-    boxes: [
+const toybox: ToyBox = {
+  name: 'toys',
+  hasKey: false,
+  boxes: [
+    {
+      name: 'A',
+      hasKey: false,
+      boxes: [],
+    },
+    {
+      name: 'B',
+      hasKey: false,
+      boxes: [
         {
-            name: "A",
-            hasKey:false,
-            boxes: []
+          name: 'C',
+          hasKey: true,
+          boxes: [
+            {
+              name: 'D',
+              hasKey: true,
+              boxes: [],
+            },
+          ],
         },
-        {
-            name: "B",
-            hasKey:false,
-            boxes: [
-                {
-                    name: "C",
-                    hasKey:true,
-                    boxes: [
-                        {
-                            name: "D",
-                            hasKey:true,
-                            boxes: []
-                        }
-                    ]
-                }
-            ]
-        }
-    ]
-}
+      ],
+    },
+  ],
+};
 
 const findKeys = (data: ToyBox) => {
-    // console.log("[STARTED]", data.name)
-    data.boxes.forEach((box) => {
-        if (box.hasKey) {
-            console.log("Found a key in box", box.name)
-            findKeys(box)
-        } else {
-            findKeys(box)
-        }
-    })
-}
+  // console.log("[STARTED]", data.name)
+  data.boxes.forEach((box) => {
+    if (box.hasKey) {
+      console.log('Found a key in box', box.name);
+      findKeys(box);
+    } else {
+      findKeys(box);
+    }
+  });
+};
 
-findKeys(toybox)
+findKeys(toybox);
